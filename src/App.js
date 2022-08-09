c58625053f93a3979ac1a5ef55d92bb501947a94
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-08-09 15:24:31 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react"
+import io from 'socket.io-client';
+const socket = io.connect("http://localhost:3001")
 
 function App() {
+  const [message, setMessage] = useState()
+  const [pastMsg, setPastMsg] = useState([])
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message: message });
+  }
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setPastMsg(data.message)
+    })
+  }, [socket])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type="text" placeholder="Send Message" onChange={e => setMessage(e.target.value)} />
+        <button onClick={sendMessage}>Send Message</button>
+        <div>
+          <h1>Send Message:</h1>
+          <p>{pastMsg}</p>
+        </div>
+      </div>
     </div>
   );
 }
