fac01549a6a54361d24e54203cc5664df84c222a
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-08-09 15:42:43 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useEffect, useState } from "react"
 import io from 'socket.io-client';
-const socket = io.connect("http://localhost:3001")
+const socket = io.connect("https://simple-chat-t.herokuapp.com/")
 
 function App() {
   const [message, setMessage] = useState()
